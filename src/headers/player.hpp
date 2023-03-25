eb63c6857b16d8b85103137c818042a8bbccb417
sspolarstar <47253708+sspolarstar@users.noreply.github.com>
2023-03-24 21:10:16 -0600

    Collision starts

--- src/headers/player.hpp
+++ src/headers/player.hpp
@@ -3,21 +3,36 @@
 #include "global.hpp"
 #include <SFML/Graphics.hpp>
 
-class Player {
-public:
-  Player(float x, float y, sf::Vector2f size);
-  void setPosition(Position pos);
-  void setPosition(float x, float y);
-  void update(float deltaTime);
-  void draw(sf::RenderWindow &window);
-  void move(Position pos);
-  Position getPosition();
-  bool hasText = 1;
-  void reduceTimeRemaing(float newTime) {
-    this->Time_Remaining = this->Time_Remaining - newTime;
-  }
-  float getTimeRemaing() { return this->Time_Remaining; }
-  void setTimeRemaining(float newTime) { this->Time_Remaining = newTime; }
+class MapManager;
+class Collision;
+
+class Player{
+    public:
+        Player(float x, float y, sf::Vector2f size);
+        void setPosition(Position pos);
+        void setPosition(float x, float y);
+        void update(float deltaTime, MapManager& mapRef);
+        void draw(sf::RenderWindow &window);
+        void move(Position pos);
+        Position getPosition();
+        bool hasText = 1;
+        void reduceTimeRemaing(float newTime){
+            this->Time_Remaining = this->Time_Remaining - newTime;
+        }
+        float getTimeRemaing(){
+            return this->Time_Remaining;
+        }
+        void setTimeRemaining(float newTime){
+            this->Time_Remaining = newTime;
+        }
+    private:
+        float Time_Remaining = 15*60;
+        float speed = 100;
+        bool facesRight = false;
+        Position position;
+        sf::RectangleShape body;
+        
+        
 
 private:
   float Time_Remaining = 100;
