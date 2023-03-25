95ddd3585c68e38b7aeae430f682069488540a79
jaredbt3 <124293430+jaredbt3@users.noreply.github.com>
2023-03-25 01:04:38 -0600

    Animation files

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -4,9 +4,30 @@ Animation::Animation() {
 
 }
 Animation::Animation(sf::Texture& texture, sf::Vector2u imageCount, float switchTime){
-    
+    uvRect.width = texture.getSize().x/imageCount.x;
+    uvRect.height = texture.getSize().y / imageCount.y;
+    this->switchTime = switchTime;
+    currentImage.x = 0;
+    totalTime = 0.0f;
 }
 
-void Animation::update(int row, float deltaTime, bool facesRight){
-
+void Animation::update(int row, sf::Vector2u rowLocater, float deltaTime, bool facesRight){
+    currentImage.y = row;
+    totalTime += deltaTime;
+    if (totalTime > this->switchTime) {
+        totalTime = 0.0f;
+        this->currentImage.x++;
+        if (this->currentImage.x > this->imageCount.x) {
+            this->currentImage.x = 0;
+        }
+        uvRect.top = currentImage.y * uvRect.height;
+        if (facesRight) {
+            uvRect.left = (currentImage.x + rowLocater.x) * uvRect.width;
+            uvRect.width = abs(uvRect.width);
+        }
+        else {
+            uvRect.left - (currentImage.x + rowLocater.x + 1) * abs(uvRect.width);
+            uvRect.width = -abs(uvRect.width);
+        }
+    }
 }
--- src/headers/Animation.hpp
+++ src/headers/Animation.hpp
@@ -8,7 +8,8 @@ class Animation {
 	public:
 		Animation();
         Animation(sf::Texture& texture, sf::Vector2u imageCount, float switchTime );
-		void update(int row, float deltaTime, bool facesRight);
+        void update(int row, sf::Vector2u rowLocater, float deltaTime, bool facesRight);
+        sf::IntRect uvRect;
         
     private:
         float switchTime;
