03008231f6a0957c184581fc338e879910fc58c0
sspolarstar <47253708+sspolarstar@users.noreply.github.com>
2023-03-24 23:21:50 -0600

    THIS IS IT FOLKES!!!!

--- src/collision.cpp
+++ src/collision.cpp
@@ -32,8 +32,9 @@ Collided map_collision(Position pos, MapManager& mapRef){
 				}
 				break;
 			}
-			output.wall[a] = Cell::wall == map.map[y][x];
-			output.nextArea[a] = Cell::stair == map.map[y][x];
+			output.wall[a] = Cell::wall == map.map[y][x] || Cell::bush == map.map[y][x] || Cell::flower == map.map[y][x] || Cell::grass == map.map[y][x];
+
+			//output.nextArea[a] = Cell::stair == map.map[y][x];
 		}
 
 	}	
