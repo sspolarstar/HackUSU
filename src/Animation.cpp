77c16610253319127b6b9f981e53b0cb82520ab9
jaredbt3 <124293430+jaredbt3@users.noreply.github.com>
2023-03-25 00:11:37 -0600

    Connecting files

---
+++ src/Animation.cpp
@@ -0,0 +1,6 @@
+#include "Animation.hpp"
+
+Animation::Animation() {
+
+}
+
--- src/headers/Animation.hpp
+++ src/headers/Animation.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include "global.hpp"
+#include "player.hpp"
+
+class Animation {
+	public:
+		Animation();
+		
+};
